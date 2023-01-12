78258b1b84afc9660a7aa867811e9ec66d29b161
leolurch <40560457+leolurch@users.noreply.github.com>
2023-01-12 21:32:04 +0100

    reduce mobile header size

--- src/routes/[[lang]]/tool/calculator/+page.svelte
+++ src/routes/[[lang]]/tool/calculator/+page.svelte
@@ -12,21 +12,23 @@
   <title>{$_('tool.calculator.title').toLocaleLowerCase()} - driftz.eu</title>
 </svelte:head>
 
-<div class="flex flex-col container mt-2">
-  <div class="flex flex-col">
-    <H1>{$_('tool.calculator.title')}</H1>
-    <p>{$_('tool.calculator.description')}</p>
+<div class="flex flex-col">
+  <div class="py-7">
+    <div class="container flex flex-col">
+      <H1>{$_('tool.calculator.title')}</H1>
+      <p class="mt-1">{$_('tool.calculator.description')}</p>
+    </div>
   </div>
 
-  <div class="grid grid-cols-1 gap-5 mt-5 md:grid-cols-3 xl:grid-cols-4">
+  <div class="grid grid-cols-1 gap-5 mt-5 md:grid-cols-3 xl:grid-cols-4 container">
     {#each Object.entries(Calculators) as [handle, calculator]}
       <Link
         href={'calculator/' + handle}
-        class="shadow-lg bg-bg-accent rounded-lg p-3 group flex flex-col hover:bg-opacity-100 bg-opacity-70 transition-opacity"
+        class=" bg-bg-accent border-white border-opacity-10 border p-5 group flex flex-col hover:bg-opacity-100 bg-opacity-70 transition-opacity"
       >
         <H2>{calculator.title[$locale]}</H2>
-        <p class="flex-grow">{calculator.description[$locale]}</p>
-        <p class="mt-3">
+        <p class="flex-grow mt-1">{calculator.description[$locale]}</p>
+        <p class="mt-5 text-lg font-medium">
           {$_(`tool.calculator.calculate_now`)}<Icon
             src={ArrowRight}
             class="w-4 inline transition-transform transform ml-1 group-hover:translate-x-2"
