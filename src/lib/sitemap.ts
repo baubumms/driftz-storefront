35fb763e6ce256e989401afb3de6e8cebcc93967
leolurch <40560457+leolurch@users.noreply.github.com>
2023-02-24 19:45:42 -0800

    trying to fix sitemap

--- src/lib/sitemap.ts
+++ src/lib/sitemap.ts
@@ -18,18 +18,10 @@ export const generateSiteMap = async (locale: LangCode) => {
     formatedCalculators()
   ]).then((blocks) => blocks.join(''));
 
-  const body = `<xml version="1.0" encoding="UTF-8">
-    <urlset
-      xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-      xmlns:xhtml="https://www.w3.org/1999/xhtml"
-      xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-      xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-      xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-    >
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${linkXml}
-    </urlset>
-    </xml>`;
+    </urlset>`;
 
   return body;
 };
@@ -39,22 +31,21 @@ const formatUrl = (localePath: string, urlEnding: string) =>
 
 const formatedTag = (url: string, updatedAt?: string, priority?: string) => {
   const xml = localeUrlHandles.map(
-    (localePath, i) => `
-      <url>
+    (localePath, i) =>
+      `<url>
         <loc>${formatUrl(localePath, url)}</loc>
         <lastmod>${updatedAt ?? new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>${priority ?? 1}</priority>
-        ${[...localeUrlHandles].splice(i, 1).map(
-          (localePath2) => `
-            <xhtml:link
-               rel="alternate"
-               hreflang="${localePath2 != '' ? localePath2.replace('/', '') : defaultLocale}"
-               href="${formatUrl(localePath2, url)}"/>
-        `
-        )}
-      </url>
-    `
+        ${[...localeUrlHandles]
+          .splice(i, 1)
+          .map(
+            (localePath2) =>
+              `<xhtml:link rel="alternate" hreflang="${
+                localePath2 != '' ? localePath2.replace('/', '') : defaultLocale
+              }" href="${formatUrl(localePath2, url)}"/>`
+          )}
+      </url>`
   );
 
   return xml.join('');
